ef89532dcd2416f3ea62795a41bf8b0d9a72f96c
Ramandeep03 <s.raman03@hotmail.com>
2024-09-02 16:11:05 +0530

    Initial Commit

---
+++ todoAppWithWidget/todoAppWithWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoAppWithWidgetApp.swift
+//  todoAppWithWidget
+//
+//  Created by Ramandeep Singh on 02/09/24.
+//
+
+import SwiftUI
+
+@main
+struct todoAppWithWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
